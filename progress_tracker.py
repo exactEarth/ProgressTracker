e7d3379dce8a24ecd7d5c87707d91be96fd2360b
Michael Overmeyer <michael.overmeyer@exactEarth.com>
2017-09-22 15:17:55 -0400

    Applied autopep8 to the rest of the repo.

--- progress_tracker.py
+++ progress_tracker.py
@@ -4,31 +4,33 @@ from datetime import datetime, timedelta
 from python_version_backports import MonkeyPatch
 MonkeyPatch.patch_total_seconds()
 
+
 class Timeout(object):
     def __init__(self, delta, start_time=None):
         self.delta = delta
         self.deadline = start_time + self.delta if start_time else datetime.utcnow() + self.delta
-        
+
     def reset(self):
         self.deadline = datetime.utcnow() + self.delta
-        
+
     def is_overdue(self):
         return self.deadline < datetime.utcnow()
-        
+
     def time_remaining(self):
         return self.deadline - datetime.utcnow()
 
+
 class ProgressTracker(object):
-    #This is a class that allows you to offload the tracking of progress.
-    #It encapsulates a number of common conditions for reporting progress.
+    # This is a class that allows you to offload the tracking of progress.
+    # It encapsulates a number of common conditions for reporting progress.
     #
-    #For example, you often want to print out your processing progress every x percent of completion, but also every y seconds.
-    #This class allows you to not have to do all of this tracking in your code. Periodically, just call the 'check' function with the iteration (1-based, not 0-based) you are on, and format the results.
+    # For example, you often want to print out your processing progress every x percent of completion, but also every y seconds.
+    # This class allows you to not have to do all of this tracking in your code. Periodically, just call the 'check' function with the iteration (1-based, not 0-based) you are on, and format the results.
     #
     def __init__(self, total=None, every_x_percent=None, every_n_records=None, every_n_seconds=None, every_n_seconds_idle=None, ignore_first_iteration=True, last_iteration=False):
 
         self.total = total
-        
+
         self.start_time = datetime.utcnow()
 
         self.every_x_percent = every_x_percent
@@ -36,7 +38,7 @@ class ProgressTracker(object):
 
         self.every_n_records = every_n_records
         self.next_record_count = every_n_records if ignore_first_iteration else 0
-        
+
         self.timeout = Timeout(timedelta(seconds=every_n_seconds)) if every_n_seconds != None else None
         self.idle_timeout = Timeout(timedelta(seconds=every_n_seconds_idle)) if every_n_seconds_idle != None else None
 
@@ -44,10 +46,10 @@ class ProgressTracker(object):
 
     def __enter__(self):
         self.start_time = datetime.utcnow()
-    
+
     def __exit__(self, type, value, traceback):
         self.complete()
-    
+
     def should_report(self, i):
         should_report = False
         if self.timeout != None and self.timeout.is_overdue():
@@ -65,31 +67,31 @@ class ProgressTracker(object):
             self.next_record_count = ((i // self.every_n_records) + 1) * self.every_n_records
         if self.total != None and self.last_iteration and i == self.total:
             should_report = True
-            
+
         return should_report
-        
-    #Returns a tuple that contains all of the usual values that you want to print out.
+
+    # Returns a tuple that contains all of the usual values that you want to print out.
     def check(self, i):
         if self.should_report(i):
             time_taken = datetime.utcnow() - self.start_time
             if self.total != None:
                 percent_complete = (i / self.total) * 100
-                estimated_time_remaining = timedelta(seconds=((100-percent_complete)/percent_complete)*time_taken.total_seconds()) if percent_complete != 0 else None
+                estimated_time_remaining = timedelta(seconds=((100 - percent_complete) / percent_complete) * time_taken.total_seconds()) if percent_complete != 0 else None
             else:
                 percent_complete = None
                 estimated_time_remaining = None
-            
-            items_per_second = i/time_taken.total_seconds() if time_taken.total_seconds() != 0 else None
-            
+
+            items_per_second = i / time_taken.total_seconds() if time_taken.total_seconds() != 0 else None
+
             result = (i, self.total, percent_complete, time_taken, estimated_time_remaining, items_per_second)
         else:
             result = None
-            
+
         if self.idle_timeout != None:
             self.idle_timeout.reset()
-            
+
         return result
-            
+
     def complete(self):
         self.end_time = datetime.utcnow()
         self.total_time = self.end_time - self.start_time
--- test_progress_tracker.py
+++ test_progress_tracker.py
@@ -3,55 +3,56 @@ import unittest
 
 from progress_tracker import ProgressTracker
 
+
 class BoundedTest(unittest.TestCase):
     def test_every_x_percent(self):
         #[5,10...100]
         NUMBER_OF_ITERATIONS = 100
-        
+
         times_reported = 0
         progress = ProgressTracker(total=NUMBER_OF_ITERATIONS, every_x_percent=5)
         with progress:
-            for i in range(0, NUMBER_OF_ITERATIONS+1):
+            for i in range(0, NUMBER_OF_ITERATIONS + 1):
                 if progress.check(i) != None:
                     times_reported += 1
         self.assertEqual(times_reported, 20)
-        
+
     def test_every_x_percent_while_including_first(self):
         #[0,5,10...100]
         NUMBER_OF_ITERATIONS = 100
-        
+
         times_reported = 0
         progress = ProgressTracker(total=NUMBER_OF_ITERATIONS, every_x_percent=5, ignore_first_iteration=False)
         with progress:
-            for i in range(0, NUMBER_OF_ITERATIONS+1):
+            for i in range(0, NUMBER_OF_ITERATIONS + 1):
                 if progress.check(i) != None:
                     times_reported += 1
         self.assertEqual(times_reported, 21)
-        
+
     def test_every_n_records(self):
         #[5,10...100]
         NUMBER_OF_ITERATIONS = 100
-        
+
         times_reported = 0
         progress = ProgressTracker(total=NUMBER_OF_ITERATIONS, every_n_records=5)
         with progress:
-            for i in range(0, NUMBER_OF_ITERATIONS+1):
+            for i in range(0, NUMBER_OF_ITERATIONS + 1):
                 if progress.check(i) != None:
                     times_reported += 1
         self.assertEqual(times_reported, 20)
-        
+
     def test_every_n_records_while_including_first(self):
         #[0,5,10...100]
         NUMBER_OF_ITERATIONS = 100
-        
+
         times_reported = 0
         progress = ProgressTracker(total=NUMBER_OF_ITERATIONS, every_n_records=5, ignore_first_iteration=False)
         with progress:
-            for i in range(0, NUMBER_OF_ITERATIONS+1):
+            for i in range(0, NUMBER_OF_ITERATIONS + 1):
                 if progress.check(i) != None:
                     times_reported += 1
         self.assertEqual(times_reported, 21)
-        
+
     def test_every_n_seconds(self):
         NUMBER_OF_ITERATIONS = 2
         SECONDS_BETWEEN_ITERATIONS = 2
@@ -59,7 +60,7 @@ class BoundedTest(unittest.TestCase):
         times_reported = 0
         progress = ProgressTracker(every_n_seconds=1)
         with progress:
-            for i in range(1, NUMBER_OF_ITERATIONS+1):
+            for i in range(1, NUMBER_OF_ITERATIONS + 1):
                 time.sleep(SECONDS_BETWEEN_ITERATIONS)
                 if progress.check(i) != None:
                     times_reported += 1
@@ -67,33 +68,33 @@ class BoundedTest(unittest.TestCase):
 
     def test_every_n_seconds_idle(self):
         IDLE_SECONDS_TRIGGER = 2
-        
-        print "Starting a test that will take {0} seconds".format((IDLE_SECONDS_TRIGGER*2) + (IDLE_SECONDS_TRIGGER+2))
+
+        print "Starting a test that will take {0} seconds".format((IDLE_SECONDS_TRIGGER * 2) + (IDLE_SECONDS_TRIGGER + 2))
         times_reported = 0
         progress = ProgressTracker(every_n_seconds_idle=IDLE_SECONDS_TRIGGER)
         with progress:
-            for i in range(IDLE_SECONDS_TRIGGER*2):
+            for i in range(IDLE_SECONDS_TRIGGER * 2):
                 time.sleep(1)
                 if progress.check(i) != None:
                     times_reported += 1
             self.assertEqual(times_reported, 0)
-            
-            time.sleep(IDLE_SECONDS_TRIGGER+2)
+
+            time.sleep(IDLE_SECONDS_TRIGGER + 2)
             if progress.check(0) != None:
-                    times_reported += 1
+                times_reported += 1
             self.assertEqual(times_reported, 1)
-            
+
     def test_every_x_percent_every_y_records(self):
         NUMBER_OF_ITERATIONS = 100
-        
+
         times_reported = 0
         progress = ProgressTracker(NUMBER_OF_ITERATIONS, every_x_percent=10, every_n_records=11)
         with progress:
-            for i in range(0, NUMBER_OF_ITERATIONS+1):
+            for i in range(0, NUMBER_OF_ITERATIONS + 1):
                 if progress.check(i) != None:
                     times_reported += 1
         self.assertEqual(times_reported, 19)
-        
+
 
 if __name__ == '__main__':
     unittest.main()
