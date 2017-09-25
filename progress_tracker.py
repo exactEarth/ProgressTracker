fd671d4cdd75044f39c1e3e32b3d668491039a3b
Michael Overmeyer <michael.overmeyer@exactEarth.com>
2017-09-25 11:24:43 -0400

    Reworked the way that ProgressTracker is used. Hopefully this is mor Pythonic now.

--- progress_tracker.py
+++ progress_tracker.py
@@ -1,4 +1,4 @@
-from __future__ import division
+from __future__ import division, print_function
 from datetime import datetime, timedelta
 
 from python_version_backports import MonkeyPatch
@@ -25,13 +25,48 @@ class ProgressTracker(object):
     # It encapsulates a number of common conditions for reporting progress.
     #
     # For example, you often want to print out your processing progress every x percent of completion, but also every y seconds.
-    # This class allows you to not have to do all of this tracking in your code. Periodically, just call the 'check' function with the iteration (1-based, not 0-based) you are on, and format the results.
+    # This class allows you to not have to do all of this tracking in your code. It will call its callback function with a formatted string.
     #
-    def __init__(self, total=None, every_x_percent=None, every_n_records=None, every_n_seconds=None, every_n_seconds_idle=None, ignore_first_iteration=True, last_iteration=False):
+    def __init__(self, iterable,
+                 total=None,
+                 callback=print,
+                 format_string=None,
+                 every_x_percent=None,
+                 every_n_records=None,
+                 every_n_seconds=None,
+                 every_n_seconds_idle=None,
+                 ignore_first_iteration=True,
+                 last_iteration=False):
 
-        self.total = total
+        self.iterable = iterable
 
-        self.start_time = datetime.utcnow()
+        try:
+            self.total = len(self.iterable)
+        except TypeError:
+            self.total = None
+
+        if self.total is None and total is not None:
+            self.total = total
+
+        if self.total is None:
+            if format_string is None:
+                self.format_string = "{i} in {time_taken}"
+
+            length_related_kwargs = ["total", "percent_complete", "estimated_time_remaining"]
+            invalid_args = [length_related_kwarg for length_related_kwarg in length_related_kwargs if "{{{0}}}".format(length_related_kwarg) in self.format_string]
+            if len(invalid_args) > 0:
+                invalid_arg_strings = ["'{0}'".format(invalid_arg) for invalid_arg in invalid_args]
+                proper_grammar = ", ".join(invalid_arg_strings[:-1]) + ', or {0}'.format(invalid_arg_strings[-1])
+                raise Exception("Format string cannot include {0} if total length is not available.".format(proper_grammar))
+
+            if every_x_percent is not None:
+                raise Exception("Cannot ask to report 'every_x_percent' if total length is not available")
+
+        else:
+            if format_string is None:
+                self.format_string = "{i}/{total} ({percent_complete}%) in {time_taken} (ETA:{estimated_time_remaining})"
+
+        self.callback = callback
 
         self.every_x_percent = every_x_percent
         self.next_percent = every_x_percent if ignore_first_iteration else 0
@@ -39,10 +74,35 @@ class ProgressTracker(object):
         self.every_n_records = every_n_records
         self.next_record_count = every_n_records if ignore_first_iteration else 0
 
-        self.timeout = Timeout(timedelta(seconds=every_n_seconds)) if every_n_seconds != None else None
-        self.idle_timeout = Timeout(timedelta(seconds=every_n_seconds_idle)) if every_n_seconds_idle != None else None
+        self.timeout = Timeout(timedelta(seconds=every_n_seconds)) if every_n_seconds is not None else None
+        self.idle_timeout = Timeout(timedelta(seconds=every_n_seconds_idle)) if every_n_seconds_idle is not None else None
 
         self.last_iteration = last_iteration
+
+        self.start_time = None
+        self.end_time = None
+        self.total_time = None
+
+        self.items_seen = 0
+        self.times_callback_called = 0
+
+    def __iter__(self):
+        with self:
+            for index, item in enumerate(self.iterable):
+                self.items_seen += 1
+                yield item
+                check = self.check(index + 1)
+                if check is not None:
+                    i, total, percent_complete, time_taken, estimated_time_remaining, items_per_second = check
+                    self.callback(self.format_string.format(
+                        i=i,
+                        total=total,
+                        percent_complete=percent_complete,
+                        time_taken=time_taken,
+                        estimated_time_remaining=estimated_time_remaining,
+                        items_per_second=items_per_second
+                    ))
+                    self.times_callback_called += 1
 
     def __enter__(self):
         self.start_time = datetime.utcnow()
@@ -52,20 +112,20 @@ class ProgressTracker(object):
 
     def should_report(self, i):
         should_report = False
-        if self.timeout != None and self.timeout.is_overdue():
+        if self.timeout is not None and self.timeout.is_overdue():
             should_report = True
             self.timeout.reset()
-        if self.idle_timeout != None and self.idle_timeout.is_overdue():
+        if self.idle_timeout is not None and self.idle_timeout.is_overdue():
             should_report = True
-        if self.total != None and self.every_x_percent != None:
+        if self.total is not None and self.every_x_percent is not None:
             percent_complete = (i / self.total) * 100
             if percent_complete >= self.next_percent:
                 should_report = True
                 self.next_percent = ((int(percent_complete) // self.every_x_percent) + 1) * self.every_x_percent
-        if self.every_n_records != None and i >= self.next_record_count:
+        if self.every_n_records is not None and i >= self.next_record_count:
             should_report = True
             self.next_record_count = ((i // self.every_n_records) + 1) * self.every_n_records
-        if self.total != None and self.last_iteration and i == self.total:
+        if self.total is not None and self.last_iteration and i == self.total:
             should_report = True
 
         return should_report
@@ -74,7 +134,7 @@ class ProgressTracker(object):
     def check(self, i):
         if self.should_report(i):
             time_taken = datetime.utcnow() - self.start_time
-            if self.total != None:
+            if self.total is not None:
                 percent_complete = (i / self.total) * 100
                 estimated_time_remaining = timedelta(seconds=((100 - percent_complete) / percent_complete) * time_taken.total_seconds()) if percent_complete != 0 else None
             else:
@@ -87,7 +147,7 @@ class ProgressTracker(object):
         else:
             result = None
 
-        if self.idle_timeout != None:
+        if self.idle_timeout is not None:
             self.idle_timeout.reset()
 
         return result
@@ -95,3 +155,7 @@ class ProgressTracker(object):
     def complete(self):
         self.end_time = datetime.utcnow()
         self.total_time = self.end_time - self.start_time
+
+
+def track_progress(iterable, **kwargs):
+    return ProgressTracker(iterable, **kwargs)
--- test_progress_tracker.py
+++ test_progress_tracker.py
@@ -1,99 +1,151 @@
 import time
 import unittest
 
-from progress_tracker import ProgressTracker
+from progress_tracker import track_progress
 
 
-class BoundedTest(unittest.TestCase):
+class BoundedTests(unittest.TestCase):
+    def setUp(self):
+        self.callback_count = 0
+
+    def increment(self):
+        self.callback_count += 1
+
     def test_every_x_percent(self):
-        #[5,10...100]
-        NUMBER_OF_ITERATIONS = 100
+        # [5,10...100]
+        NUMBER_OF_ITERATIONS = 101
 
-        times_reported = 0
-        progress = ProgressTracker(total=NUMBER_OF_ITERATIONS, every_x_percent=5)
-        with progress:
-            for i in range(0, NUMBER_OF_ITERATIONS + 1):
-                if progress.check(i) != None:
-                    times_reported += 1
-        self.assertEqual(times_reported, 20)
+        results = list(track_progress(range(0, NUMBER_OF_ITERATIONS), every_x_percent=5, callback=lambda _: self.increment()))
+        self.assertEqual(len(results), NUMBER_OF_ITERATIONS)
+        self.assertEqual(self.callback_count, 20)
 
     def test_every_x_percent_while_including_first(self):
-        #[0,5,10...100]
-        NUMBER_OF_ITERATIONS = 100
+        # [0,5,10...100]
+        NUMBER_OF_ITERATIONS = 101
 
-        times_reported = 0
-        progress = ProgressTracker(total=NUMBER_OF_ITERATIONS, every_x_percent=5, ignore_first_iteration=False)
-        with progress:
-            for i in range(0, NUMBER_OF_ITERATIONS + 1):
-                if progress.check(i) != None:
-                    times_reported += 1
-        self.assertEqual(times_reported, 21)
+        results = list(track_progress(range(0, NUMBER_OF_ITERATIONS), every_x_percent=5, ignore_first_iteration=False, callback=lambda _: self.increment()))
+        self.assertEqual(len(results), NUMBER_OF_ITERATIONS)
+        self.assertEqual(self.callback_count, 21)
 
     def test_every_n_records(self):
-        #[5,10...100]
-        NUMBER_OF_ITERATIONS = 100
+        # [5,10...100]
+        NUMBER_OF_ITERATIONS = 101
 
-        times_reported = 0
-        progress = ProgressTracker(total=NUMBER_OF_ITERATIONS, every_n_records=5)
-        with progress:
-            for i in range(0, NUMBER_OF_ITERATIONS + 1):
-                if progress.check(i) != None:
-                    times_reported += 1
-        self.assertEqual(times_reported, 20)
+        results = list(track_progress(range(0, NUMBER_OF_ITERATIONS), every_n_records=5, callback=lambda _: self.increment()))
+        self.assertEqual(len(results), NUMBER_OF_ITERATIONS)
+        self.assertEqual(self.callback_count, 20)
 
     def test_every_n_records_while_including_first(self):
-        #[0,5,10...100]
-        NUMBER_OF_ITERATIONS = 100
+        # [0,5,10...100]
+        NUMBER_OF_ITERATIONS = 101
 
-        times_reported = 0
-        progress = ProgressTracker(total=NUMBER_OF_ITERATIONS, every_n_records=5, ignore_first_iteration=False)
-        with progress:
-            for i in range(0, NUMBER_OF_ITERATIONS + 1):
-                if progress.check(i) != None:
-                    times_reported += 1
-        self.assertEqual(times_reported, 21)
+        results = list(track_progress(range(0, NUMBER_OF_ITERATIONS), every_n_records=5, ignore_first_iteration=False, callback=lambda _: self.increment()))
+        self.assertEqual(len(results), NUMBER_OF_ITERATIONS)
+        self.assertEqual(self.callback_count, 21)
 
     def test_every_n_seconds(self):
         NUMBER_OF_ITERATIONS = 2
         SECONDS_BETWEEN_ITERATIONS = 2
         print "Starting a test that will take {0} seconds".format(NUMBER_OF_ITERATIONS * SECONDS_BETWEEN_ITERATIONS)
-        times_reported = 0
-        progress = ProgressTracker(every_n_seconds=1)
-        with progress:
-            for i in range(1, NUMBER_OF_ITERATIONS + 1):
-                time.sleep(SECONDS_BETWEEN_ITERATIONS)
-                if progress.check(i) != None:
-                    times_reported += 1
-        self.assertEqual(times_reported, 2)
+        for _ in track_progress(range(1, NUMBER_OF_ITERATIONS + 1), every_n_seconds=1, callback=lambda _: self.increment()):
+            time.sleep(SECONDS_BETWEEN_ITERATIONS)
+        self.assertEqual(self.callback_count, 2)
 
     def test_every_n_seconds_idle(self):
         IDLE_SECONDS_TRIGGER = 2
 
-        print "Starting a test that will take {0} seconds".format((IDLE_SECONDS_TRIGGER * 2) + (IDLE_SECONDS_TRIGGER + 2))
-        times_reported = 0
-        progress = ProgressTracker(every_n_seconds_idle=IDLE_SECONDS_TRIGGER)
-        with progress:
-            for i in range(IDLE_SECONDS_TRIGGER * 2):
-                time.sleep(1)
-                if progress.check(i) != None:
-                    times_reported += 1
-            self.assertEqual(times_reported, 0)
+        print "Starting a test that will take {0} seconds".format((IDLE_SECONDS_TRIGGER * 2) - 1 + (IDLE_SECONDS_TRIGGER + 2))
 
-            time.sleep(IDLE_SECONDS_TRIGGER + 2)
-            if progress.check(0) != None:
-                times_reported += 1
-            self.assertEqual(times_reported, 1)
+        for i in track_progress(range(1, IDLE_SECONDS_TRIGGER * 2), every_n_seconds_idle=IDLE_SECONDS_TRIGGER, callback=lambda _: self.increment()):
+            time.sleep(1)
+            if i == 1:
+                self.assertEqual(self.callback_count, 0)
+            elif i == 2:
+                time.sleep(IDLE_SECONDS_TRIGGER + 2)
+        self.assertEqual(self.callback_count, 1)
 
     def test_every_x_percent_every_y_records(self):
         NUMBER_OF_ITERATIONS = 100
 
-        times_reported = 0
-        progress = ProgressTracker(NUMBER_OF_ITERATIONS, every_x_percent=10, every_n_records=11)
-        with progress:
-            for i in range(0, NUMBER_OF_ITERATIONS + 1):
-                if progress.check(i) != None:
-                    times_reported += 1
-        self.assertEqual(times_reported, 19)
+        results = list(track_progress(range(0, NUMBER_OF_ITERATIONS), every_x_percent=10, every_n_records=11, callback=lambda _: self.increment()))
+        self.assertEqual(len(results), NUMBER_OF_ITERATIONS)
+        self.assertEqual(self.callback_count, 19)
+
+    def test_record_keeping(self):
+        NUMBER_OF_ITERATIONS = 100
+
+        pt = track_progress(range(0, NUMBER_OF_ITERATIONS), every_x_percent=10, every_n_records=11, callback=lambda _: self.increment())
+        results = list(pt)
+        self.assertEqual(len(results), NUMBER_OF_ITERATIONS)
+        self.assertEqual(len(results), pt.items_seen)
+        self.assertEqual(self.callback_count, 19)
+        self.assertEqual(self.callback_count, pt.times_callback_called)
+        self.assertNotEqual(pt.start_time, None)
+        self.assertNotEqual(pt.end_time, None)
+        self.assertNotEqual(pt.total_time, None)
+
+
+class UnboundedTests(unittest.TestCase):
+    def setUp(self):
+        self.callback_count = 0
+
+    def increment(self):
+        self.callback_count += 1
+
+    def test_every_x_percent(self):
+        # [5,10...100]
+        NUMBER_OF_ITERATIONS = 101
+
+        # every_x_percent doesn't make sense when you don't know the size (because it is a generator)
+        with self.assertRaises(Exception):
+            track_progress((i for i in range(0, NUMBER_OF_ITERATIONS)), every_x_percent=5, callback=lambda _: self.increment())
+
+        # But if you happen to know the size a priori, you can pass it in
+        results = list(track_progress((i for i in range(0, NUMBER_OF_ITERATIONS)), total=NUMBER_OF_ITERATIONS, every_x_percent=5, callback=lambda _: self.increment()))
+        self.assertEqual(len(results), NUMBER_OF_ITERATIONS)
+        self.assertEqual(self.callback_count, 20)
+
+    def test_every_n_records(self):
+        # [5,10...100]
+        NUMBER_OF_ITERATIONS = 101
+
+        results = list(track_progress((i for i in range(0, NUMBER_OF_ITERATIONS)), every_n_records=5, callback=lambda _: self.increment()))
+        self.assertEqual(len(results), NUMBER_OF_ITERATIONS)
+        self.assertEqual(self.callback_count, 20)
+
+    def test_every_n_seconds(self):
+        NUMBER_OF_ITERATIONS = 2
+        SECONDS_BETWEEN_ITERATIONS = 2
+        print "Starting a test that will take {0} seconds".format(NUMBER_OF_ITERATIONS * SECONDS_BETWEEN_ITERATIONS)
+        for _ in track_progress((i for i in range(1, NUMBER_OF_ITERATIONS + 1)), every_n_seconds=1, callback=lambda _: self.increment()):
+            time.sleep(SECONDS_BETWEEN_ITERATIONS)
+        self.assertEqual(self.callback_count, 2)
+
+    def test_every_n_seconds_idle(self):
+        IDLE_SECONDS_TRIGGER = 2
+
+        print "Starting a test that will take {0} seconds".format((IDLE_SECONDS_TRIGGER * 2) - 1 + (IDLE_SECONDS_TRIGGER + 2))
+
+        for i in track_progress((i for i in range(1, IDLE_SECONDS_TRIGGER * 2)), every_n_seconds_idle=IDLE_SECONDS_TRIGGER, callback=lambda _: self.increment()):
+            time.sleep(1)
+            if i == 1:
+                self.assertEqual(self.callback_count, 0)
+            elif i == 2:
+                time.sleep(IDLE_SECONDS_TRIGGER + 2)
+        self.assertEqual(self.callback_count, 1)
+
+    def test_every_x_percent_every_y_records(self):
+        NUMBER_OF_ITERATIONS = 100
+
+        results = list(track_progress((i for i in range(0, NUMBER_OF_ITERATIONS)), total=NUMBER_OF_ITERATIONS, every_x_percent=10, every_n_records=11, callback=lambda _: self.increment()))
+        self.assertEqual(len(results), NUMBER_OF_ITERATIONS)
+        self.assertEqual(self.callback_count, 19)
+
+    def test_format_strings(self):
+        with self.assertRaises(Exception):
+            track_progress((i for i in range(0, 100)), format_string="{percent_complete}", every_n_records=11, callback=lambda _: self.increment())
+
+        track_progress((i for i in range(0, 100)), total=100, format_string="{percent_complete}", every_n_records=11, callback=lambda _: self.increment())
 
 
 if __name__ == '__main__':
