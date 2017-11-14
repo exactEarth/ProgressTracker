9f5ce8c60f24c869dbdfd23c9d99e68e5354b63e
Michael Overmeyer <michael.overmeyer@exactEarth.com>
2017-11-14 16:05:47 -0500

    Move `Timeout` to its own file so others could use it.

--- progress_tracker.py
+++ progress_tracker.py
@@ -1,23 +1,9 @@
 from __future__ import division, print_function
 from datetime import datetime, timedelta
 
+from ee_libs.timeout import Timeout
 from python_version_backports import MonkeyPatch
 MonkeyPatch.patch_total_seconds()
-
-
-class Timeout(object):
-    def __init__(self, delta, start_time=None):
-        self.delta = delta
-        self.deadline = start_time + self.delta if start_time else datetime.utcnow() + self.delta
-
-    def reset(self):
-        self.deadline = datetime.utcnow() + self.delta
-
-    def is_overdue(self):
-        return self.deadline < datetime.utcnow()
-
-    def time_remaining(self):
-        return self.deadline - datetime.utcnow()
 
 
 class ProgressTracker(object):
---
+++ timeout.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+
+class Timeout(object):
+    def __init__(self, delta, start_time=None):
+        self.delta = delta
+        self.deadline = start_time + self.delta if start_time else datetime.utcnow() + self.delta
+
+    def reset(self):
+        self.deadline = datetime.utcnow() + self.delta
+
+    def is_overdue(self):
+        return self.deadline < datetime.utcnow()
+
+    def time_remaining(self):
+        return self.deadline - datetime.utcnow()
