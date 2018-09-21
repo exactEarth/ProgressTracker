74e3ecea7bd0fbb4ab86d7fd9c69ea955ce4ed5a
nick.gerow@exactearth.com <nick.gerow@exactearth.com>
2018-09-21 15:42:14 -0400

    First attempt at CD through circle-ci

--- setup.py
+++ setup.py
@@ -1,11 +1,39 @@
 #!/usr/bin/env python
+# From https://circleci.com/blog/continuously-deploying-python-packages-to-pypi-with-circleci/
+import os
+import sys
+
 from setuptools import setup
+from setuptools.command.install import install
+
+VERSION = "1.0.0"
+
+
+def readme() -> str:
+    """print long description"""
+    with open('README.rst') as f:
+        return f.read()
+
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self) -> None:
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
 
 setup(
     name='progress_tracker',
-    version='1.0.0',
+    version=VERSION,
     description='A utility that wraps an Iterable and regularly prints out progress on the processing of that Iterable',
-    long_description="A utility that wraps an Iterable and regularly prints out progress on the processing of that Iterable",
+    long_description=readme(),
     author='exactEarth Ltd.',
     author_email='open-source@exactearth.com',
 
@@ -27,5 +55,9 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
     keywords='',
-    license='MIT'
+    license='MIT',
+    python_requires='>=3.6',
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    }
 )
